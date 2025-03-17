52af6943104a8182e2609875be340bf49d398ca1
claudiopatti <claudiopatti1997@gmail.com>
2025-03-17 15:10:07 +0100

    continua disposizione sezione main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -31,14 +31,73 @@ export default {
 
 <div class="container Second-main">
     <div class="row">
-        <div class="col">
-            <h2>Skills</h2>
+        <div class="col column-Skills">
+            <h2 class="py-4">Skills</h2>
 
-            <ul>
-                <li v-for="skill in store.skills" :key="skill.id">
+            <div class="row pb-4">
+                <div class="col-4" v-for="skill in store.skills" :key="skill.id">
                     {{ skill.name }}
-                </li>
-            </ul>
+                    <span v-html="skill.icon"></span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container Third-main">
+    <div class="row">
+        <div class="col column-Skills">
+            <h2 class="py-4">Progetti</h2>
+
+            <div class="row pb-4">
+                <div class="col-4" v-for="skill in store.skills" :key="skill.id">
+                    {{ skill.name }}
+                    <span v-html="skill.icon"></span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container Quarter-main">
+    <div class="row">
+        <div class="col column-Skills">
+            <h2 class="py-4">Chi sono</h2>
+
+            <div>
+                <p>
+                    Descrizione
+                </p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container Fifth-main ">
+
+    <div class="row ">
+        <div class="col column-Skills  ">
+            <h1 class="py-4 d-flex justify-content-center">Contatti</h1>
+
+            
+            <div class="d-flex justify-content-center">
+                <i class="fa-solid fa-share-nodes icon-contact"></i>
+            </div>
+
+            <div >
+                <p class="d-flex justify-content-center">
+                    <i class="fa-solid fa-envelope pt-1 pe-2"></i> Email: claudiopatti1997@gmail.com
+                </p>
+                <p class="d-flex justify-content-center">
+                    <i class="fa-brands fa-github pt-1 pe-2"></i> Github: <a href="https://github.com/claudiopatti" class="text-decoration-none">Claudio Patti</a>
+                </p>
+                <p class="d-flex justify-content-center">
+                    <i class="fa-brands fa-linkedin pt-1 pe-2"></i> Linkedin: <a href="https://www.linkedin.com/in/claudio-patti1997/" class="text-decoration-none">Claudio Patti</a>
+                </p>
+            </div>
 
         </div>
     </div>
@@ -52,7 +111,20 @@ export default {
     height: 500px;
 }
 
-.Second-main{
+.Second-main, .Third-main, .Quarter-main, .Fifth-main {
     height: 400px;
+    width: 100%;
+
+    .column-Skills{
+        background-color: #f0f0f0;
+    }
+}
+
+.Fifth-main{
+
+    .icon-contact{
+        font-size: 500px;
+        color: #0a72d5;
+    }
 }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -3,17 +3,17 @@ import { reactive } from 'vue';
 
 const iMieiDati = reactive({
     skills : [
-        {id: 1, name: 'HTML', level: 3},
-        {id: 2, name: 'CSS', level: 3},
-        {id: 3, name: 'SCSS', level: 2},
-        {id: 4, name: 'JavaScript', level: 2},
-        {id: 5, name: 'Vue.js', level: 2},
-        {id: 6, name: 'PHP', level: 2},
-        {id: 7, name: 'MySQL', level: 2},
-        {id: 8, name: 'Laravel', level: 2},
-        {id: 9, name: 'Bootstrap', level: 2},
-        {id: 10, name: 'Vite', level: 2},
-        {id: 11, name: 'Postman', level: 2},
+        {id: 1, name: 'HTML', level: 3, icon: '<i class="fa-brands fa-html5"></i>'},
+        {id: 2, name: 'CSS', level: 3, icon: '<i class="fa-brands fa-css"></i>'},
+        {id: 3, name: 'SCSS', level: 2, icon: '<i class="fa-brands fa-sass"></i>'},
+        {id: 4, name: 'JavaScript', level: 2, icon: '<i class="fa-brands fa-js"></i>'},
+        {id: 5, name: 'Vue.js', level: 2, icon: '<i class="fa-brands fa-vuejs"></i>'},
+        {id: 6, name: 'PHP', level: 2, icon: '<i class="fa-brands fa-php"></i>'},
+        {id: 7, name: 'Laravel', level: 2, icon: '<i class="fa-brands fa-laravel"></i>'},
+        {id: 8, name: 'Bootstrap', level: 2, icon: '<i class="fa-brands fa-bootstrap"></i>'},
+        {id: 9, name: 'MySQL', level: 2, icon: '<i class="fa-brands fa-mysql"></i>'},
+        {id: 10, name: 'Vite', level: 2, icon: '<i class="fa-brands fa-vite"></i>'},
+        {id: 11, name: 'Postman', level: 2, icon: '<i class="fa-brands fa-postman"></i>'},
     ]
 });
 
