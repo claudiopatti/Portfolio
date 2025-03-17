b92f2c8116afcfc98c6ded11da27ce82eb32a1f0
claudiopatti <claudiopatti1997@gmail.com>
2025-03-17 13:41:17 +0100

    Inizio riempimento main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -16,20 +16,43 @@ export default {
 
 <template>
 
-  <div class="container pt-5">
+<div class="container First-main pt-5">
     <div class="row">
-      <div class="col-6 text-white">
-        <h1>Claudio Patti</h1>
+        <div class="col-6 text-white">
+            <h1>Claudio Patti</h1>
 
-        <h3>Jr Full Stack Developer</h3>
-      </div>
-      <div class="col-6 text-white">
-        <h1>Foto Mia Carina</h1>
-      </div>
+            <h3>Jr Full Stack Developer</h3>
+        </div>
+        <div class="col-6 text-white">
+            <h1>Foto Mia Carina</h1>
+        </div>
     </div>
-  </div>
+</div>
+
+<div class="container Second-main">
+    <div class="row">
+        <div class="col">
+            <h2>Skills</h2>
+
+            <ul>
+                <li v-for="skill in store.skills" :key="skill.id">
+                    {{ skill.name }}
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</div>
 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.First-main {
+    height: 500px;
+}
+
+.Second-main{
+    height: 400px;
+}
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,19 @@ import { reactive } from 'vue';
 
 
 const iMieiDati = reactive({
-    name: 'micol'
+    skills : [
+        {id: 1, name: 'HTML', level: 3},
+        {id: 2, name: 'CSS', level: 3},
+        {id: 3, name: 'SCSS', level: 2},
+        {id: 4, name: 'JavaScript', level: 2},
+        {id: 5, name: 'Vue.js', level: 2},
+        {id: 6, name: 'PHP', level: 2},
+        {id: 7, name: 'MySQL', level: 2},
+        {id: 8, name: 'Laravel', level: 2},
+        {id: 9, name: 'Bootstrap', level: 2},
+        {id: 10, name: 'Vite', level: 2},
+        {id: 11, name: 'Postman', level: 2},
+    ]
 });
 
 export default iMieiDati
