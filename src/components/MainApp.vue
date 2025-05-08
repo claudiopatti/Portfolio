ba2c2295781e4b1abfaf56550c69f8786ffa554a
claudiopatti <claudiopatti1997@gmail.com>
2025-05-08 14:09:53 +0200

    sistemazione titoli e layout main

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -39,7 +39,7 @@ export default {
     <div class="row">
         <div class="col column-Skills">
 
-            <h1 class="text-center fs-1 py-2">Competenze </h1>
+            <h1 class="text-center py-2 pt-sans-bold">COMPET<span class="text-primary">ENZE</span>  </h1>
             <div class="row">
                 <div @click="changeSkillFront()" class="col-6">
 
@@ -113,9 +113,10 @@ export default {
 <div class="container pt-5 Third-main" id="third-main">
     <div class="row">
         <div class="col column-project">
-            <h2 class="py-4 text-center">Progetti</h2>
+            <h1 class="py-4 text-center pt-sans-bold">PROGE<span class="text-primary">TTI</span></h1>
 
             <div class="row card-project">
+                <img class="px-0" src="../assets/immagine di Boolbnb.png" alt="Immagine-Boolbnb">
                 
             </div>
 
@@ -126,11 +127,26 @@ export default {
 <div class="container pt-5 Quarter-main" id="quarter-main">
     <div class="row">
         <div class="col column-about">
-            <h2 class="py-4">Chi sono</h2>
+            <h2 class="py-4 pt-sans-bold">CHI <span class="text-primary">SONO</span></h2>
 
             <div>
-                <p>
-                    Descrizione
+                <p class="description-design">
+                    Buongiorno, Mi presento sono Claudio Patti. <br>
+                    Sono un junior web developer che ha alle spalle ha varie esperienze lavorative in
+                    diversi settori e che grazie a queste esperienze lavorative hai acquisito competenze
+                    legate all'ambiente lavorativo e al lavoro di squadra, come ad esempio problem
+                    solving,la capacità di organizzazione e lavoro in team.<br> 
+                    Dopo il trasferimento a Bologna
+                    ho deciso di seguire la mia passione per la programmazione, investendo in un corso
+                    che mi avrebbe potuto dare una nuova carriera che allo stesso tempo mi piacesse e ho
+                    deciso di fare il corso Boolean, completandolo con successo. Durante il corso, ho
+                    acquisito competenze in sviluppo base (HTML, CSS) sviluppo front-end (JavaScript,
+                    Vue.js) e back-end (PHP, Laravel), lavorando anche su progetti di gruppo, come ad
+                    esempio, la replica del sito Airbnb. Alla conclusione del corso mi sono reso conto che
+                    da quando ho iniziato a scrivere le prime righe di codice in HTML, avere un riscontro
+                    visivo e immediato nella mia pagina web mi piaceva molto soprattutto andare a
+                    ricreare i layout proposti o da noi pensati e poi vederli prendere forma pian piano.
+                    questo mi ha fatto dire sì, è il lavoro che vorrei iniziare a fare.
                 </p>
             </div>
 
@@ -138,26 +154,26 @@ export default {
     </div>
 </div>
 
-<div class="container pt-5 Fifth-main " id="fifth-main">
+<div class="container py-5  Fifth-main " id="fifth-main">
 
     <div class="row ">
         <div class="col column-contact  ">
-            <h1 class="py-4 d-flex justify-content-center">Contatti</h1>
+            <h1 class="py-4 d-flex justify-content-center pt-sans-bold">CONTA <span class="text-primary">TTI</span></h1>
 
             
             <div class="d-flex justify-content-center">
                 <i class="fa-solid fa-share-nodes icon-contact"></i>
             </div>
 
-            <div >
-                <p class="d-flex justify-content-center">
-                    <i class="fa-solid fa-envelope pt-1 pe-2"></i> Email: claudiopatti1997@gmail.com
+            <div class="py-4">
+                <p class="d-flex justify-content-center fs-2">
+                    <i class="fa-solid fa-envelope pt-2 pe-2"></i> Email: claudiopatti1997@gmail.com
                 </p>
-                <p class="d-flex justify-content-center">
-                    <i class="fa-brands fa-github pt-1 pe-2"></i> Github:&nbsp; <a href="https://github.com/claudiopatti" class="text-decoration-none">Claudio Patti</a>
+                <p class="d-flex justify-content-center fs-2">
+                    <i class="fa-brands fa-github pt-2 pe-2"></i> Github:&nbsp; <a href="https://github.com/claudiopatti" class="text-decoration-none">Claudio Patti</a>
                 </p>
-                <p class="d-flex justify-content-center">
-                    <i class="fa-brands fa-linkedin pt-1 pe-2"></i> Linkedin:&nbsp; <a href="https://www.linkedin.com/in/claudio-patti1997/" class="text-decoration-none">Claudio Patti</a>
+                <p class="d-flex justify-content-center fs-2">
+                    <i class="fa-brands fa-linkedin pt-2 pe-2"></i> Linkedin:&nbsp; <a href="https://www.linkedin.com/in/claudio-patti1997/" class="text-decoration-none">Claudio Patti</a>
                 </p>
             </div>
 
@@ -173,19 +189,24 @@ export default {
     height: 500px;
 }
 
-.Second-main, .Third-main, .Quarter-main, .Fifth-main {
-    // height: 400px;
+.Second-main{
+    height: 700px;
+}
+
+.Second-main, .Third-main, .Quarter-main {
+    // height: 500px;
     width: 100%;
     cursor: pointer;
+    // margin-top: 30px;
 
     .column-Skills{
         background-color: #f0f0f0;
-        height: 100%;
+        height: 600px;
         .col-6, .col {
             padding: 0;
             // height: 100%;
             & .container-name-skill{
-                height: 100%;
+                height: 400px;
                 padding-left: 10px;
                 padding-top: 10px;
                 background-color: #0a72d5;
@@ -193,11 +214,6 @@ export default {
                 font-size: 30px;
                 font-weight: bold;
             }
-        }
-
-        .col-6{
-            padding: 0;
-
             .button-skill-click {
                 border: 10px solid grey;
                 background-color: #0a72d5;
@@ -216,6 +232,23 @@ export default {
             background-color: #0a72d5;
         }
     }
+
+    .column-about {
+        background-color: #f0f0f0;
+        height: 800px;
+
+        .description-design{
+            padding: 20px;
+            font-size: 20px;
+        }
+
+        // .card-project {
+        //     width: 1200px;
+        //     height: 600px;
+        //     margin: 0px auto;
+        //     background-color: #0a72d5;
+        // }
+    }
 }
 
 .Fifth-main{
@@ -223,6 +256,11 @@ export default {
     .icon-contact{
         font-size: 500px;
         color: #0a72d5;
+    }
+
+    .column-contact {
+        background-color: #f0f0f0;
+        // height: 800px;
     }
 }
 
