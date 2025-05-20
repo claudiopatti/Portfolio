950fa4e8902bbf35d05eadbfe7054c772363a73e
claudiopatti <claudiopatti1997@gmail.com>
2025-05-20 18:34:36 +0200

    aggiornato

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -16,55 +16,79 @@ export default {
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-2 d-flex">
-                <div class="header-left ">
-                    <!-- <a href="home">
-                        <img src="logo.png" alt="Logo">
-                    </a> -->
-                    <h1 class="text-white">Logo</h1>
+    <div class="bg-black pt-4">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 d-flex align-items-center">
+                    <div class="header-left ">
+                        <!-- <a href="home">
+                            <img src="logo.png" alt="Logo">
+                        </a> -->
+                        <h1 class="text-white">Logo</h1>
+                    </div>
                 </div>
-            </div>
-            <div class="col-10 d-flex justify-content-end align-items-center">
-                <div class="mx-2">
-                    <a href="#" class="text-decoration-none text-white">
-                        Home
-                    </a>
-                </div>
-                <div class="mx-2">
-                    <a href="#second-main" class="text-decoration-none text-white">
-                        Competenze Tecniche
-                    </a>
-                </div>
-                <div class="mx-2">
-                    <a href="#third-main" class="text-decoration-none text-white">
-                        Progetti
-                    </a>
-                </div>
-                <div class="mx-2">
-                    <a href="#quarter-main" class="text-decoration-none text-white">
-                        Chi sono
-                    </a>
-                </div>
-                <div class="mx-2" >
-                    <a href="#fifth-main" class="text-decoration-none text-white">
-                        Contatti
-                    </a>
-                </div>
-                <!-- <div>
-                    <a href="home" class="text-decoration-none text-dark">
-                        HOME
-                    </a>
-                </div>
-                <div class="header-left text-danger">
-                    <p class="">{{ store.name }}</p>
-                    
-                </div> -->
-            </div>
 
+                
+                <div class="col-10 d-flex justify-content-end align-items-center  ">
+
+                    <div class="dropdown d-sm-none d-block d-flex justify-content-end align-items-center ">
+                        <a class="btn btn-black dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Menu
+                        </a>
+    
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Home</a></li>
+                            <li><a class="dropdown-item" href="#second-main">Competenze Tecniche</a></li>
+                            <li><a class="dropdown-item" href="#third-main">Progetti</a></li>
+                            <li><a class="dropdown-item" href="#quarter-main">Chi sono</a></li>
+                            <li><a class="dropdown-item" href="#fifth-main">Contatti</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="d-sm-flex justify-content-end align-items-center d-none d-sm-block">
+
+                        <div class="mx-2">
+                            <a href="#" class="text-decoration-none text-white">
+                                Home
+                            </a>
+                        </div>
+                        <div class="mx-2">
+                            <a href="#second-main" class="text-decoration-none text-white">
+                                Competenze Tecniche
+                            </a>
+                        </div>
+                        <div class="mx-2">
+                            <a href="#third-main" class="text-decoration-none text-white">
+                                Progetti
+                            </a>
+                        </div>
+                        <div class="mx-2">
+                            <a href="#quarter-main" class="text-decoration-none text-white">
+                                Chi sono
+                            </a>
+                        </div>
+                        <div class="mx-2" >
+                            <a href="#fifth-main" class="text-decoration-none text-white">
+                                Contatti
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <!-- <div>
+                        <a href="home" class="text-decoration-none text-dark">
+                            HOME
+                        </a>
+                    </div>
+                    <div class="header-left text-danger">
+                        <p class="">{{ store.name }}</p>
+                        
+                    </div> -->
+                </div>
+    
+            </div>
+    
         </div>
-
     </div>
 
 </template>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -21,19 +21,22 @@ export default {
 </script>
 
 <template>
-
-<div class="container mt-5 First-main pt-5">
-    <div class="row">
-        <div class="col-6 text-white">
-            <h1>Claudio Patti</h1>
-
-            <h3>Jr Full Stack Developer</h3>
+    <div class="bg-black">
+        <div class="container First-main">
+            <div class="row">
+                <div class="col-6 text-white">
+                    <h1>Claudio Patti</h1>
+        
+                    <h3>Jr Full Stack Developer</h3>
+                </div>
+                <div class="col-6 text-white">
+                    <h1>Foto Mia Carina</h1>
+                </div>
+            </div>
         </div>
-        <div class="col-6 text-white">
-            <h1>Foto Mia Carina</h1>
-        </div>
+        
     </div>
-</div>
+
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="image-up-layout-competenze"><path fill="#CAF0F8" fill-opacity="1" d="M0,192L84.7,256L169.4,32L254.1,256L338.8,288L423.5,224L508.2,224L592.9,160L677.6,224L762.4,128L847.1,192L931.8,192L1016.5,32L1101.2,160L1185.9,160L1270.6,64L1355.3,160L1440,192L1440,320L1355.3,320L1270.6,320L1185.9,320L1101.2,320L1016.5,320L931.8,320L847.1,320L762.4,320L677.6,320L592.9,320L508.2,320L423.5,320L338.8,320L254.1,320L169.4,320L84.7,320L0,320Z"></path></svg>
 
@@ -44,7 +47,7 @@ export default {
 
             <h1 class="text-center py-2 pt-sans-bold">COMPET<span class="text-title">ENZE</span>  </h1>
 
-            <div class="container pb-3">
+            <div class="container pb-5 mb-3">
 
                 <div class="row">
                     <div @click="changeSkillFront()" class="col-6 ">
@@ -165,7 +168,7 @@ export default {
     </div>
 </div>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="image-up-layout-chisono"><path fill="#CAF0F8" fill-opacity="1" d="M0,128L14.1,112C28.2,96,56,64,85,80C112.9,96,141,160,169,170.7C197.6,181,226,139,254,101.3C282.4,64,311,32,339,69.3C367.1,107,395,213,424,256C451.8,299,480,277,508,245.3C536.5,213,565,171,593,138.7C621.2,107,649,85,678,96C705.9,107,734,149,762,160C790.6,171,819,149,847,149.3C875.3,149,904,171,932,197.3C960,224,988,256,1016,234.7C1044.7,213,1073,139,1101,106.7C1129.4,75,1158,85,1186,90.7C1214.1,96,1242,96,1271,106.7C1298.8,117,1327,139,1355,170.7C1383.5,203,1412,245,1426,266.7L1440,288L1440,320L1425.9,320C1411.8,320,1384,320,1355,320C1327.1,320,1299,320,1271,320C1242.4,320,1214,320,1186,320C1157.6,320,1129,320,1101,320C1072.9,320,1045,320,1016,320C988.2,320,960,320,932,320C903.5,320,875,320,847,320C818.8,320,791,320,762,320C734.1,320,706,320,678,320C649.4,320,621,320,593,320C564.7,320,536,320,508,320C480,320,452,320,424,320C395.3,320,367,320,339,320C310.6,320,282,320,254,320C225.9,320,198,320,169,320C141.2,320,113,320,85,320C56.5,320,28,320,14,320L0,320Z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="image-up-layout-chisono"><path class="pb-2" fill="#CAF0F8" fill-opacity="1" d="M0,128L14.1,112C28.2,96,56,64,85,80C112.9,96,141,160,169,170.7C197.6,181,226,139,254,101.3C282.4,64,311,32,339,69.3C367.1,107,395,213,424,256C451.8,299,480,277,508,245.3C536.5,213,565,171,593,138.7C621.2,107,649,85,678,96C705.9,107,734,149,762,160C790.6,171,819,149,847,149.3C875.3,149,904,171,932,197.3C960,224,988,256,1016,234.7C1044.7,213,1073,139,1101,106.7C1129.4,75,1158,85,1186,90.7C1214.1,96,1242,96,1271,106.7C1298.8,117,1327,139,1355,170.7C1383.5,203,1412,245,1426,266.7L1440,288L1440,320L1425.9,320C1411.8,320,1384,320,1355,320C1327.1,320,1299,320,1271,320C1242.4,320,1214,320,1186,320C1157.6,320,1129,320,1101,320C1072.9,320,1045,320,1016,320C988.2,320,960,320,932,320C903.5,320,875,320,847,320C818.8,320,791,320,762,320C734.1,320,706,320,678,320C649.4,320,621,320,593,320C564.7,320,536,320,508,320C480,320,452,320,424,320C395.3,320,367,320,339,320C310.6,320,282,320,254,320C225.9,320,198,320,169,320C141.2,320,113,320,85,320C56.5,320,28,320,14,320L0,320Z"></path></svg>
 
 <div class="Quarter-main" id="quarter-main">
     <div class="row g-0">
@@ -202,33 +205,37 @@ export default {
     </div>
 </div>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="image-down-layout-chisono"><path fill="#CAF0F8" fill-opacity="1" d="M0,128L14.1,112C28.2,96,56,64,85,80C112.9,96,141,160,169,170.7C197.6,181,226,139,254,101.3C282.4,64,311,32,339,69.3C367.1,107,395,213,424,256C451.8,299,480,277,508,245.3C536.5,213,565,171,593,138.7C621.2,107,649,85,678,96C705.9,107,734,149,762,160C790.6,171,819,149,847,149.3C875.3,149,904,171,932,197.3C960,224,988,256,1016,234.7C1044.7,213,1073,139,1101,106.7C1129.4,75,1158,85,1186,90.7C1214.1,96,1242,96,1271,106.7C1298.8,117,1327,139,1355,170.7C1383.5,203,1412,245,1426,266.7L1440,288L1440,320L1425.9,320C1411.8,320,1384,320,1355,320C1327.1,320,1299,320,1271,320C1242.4,320,1214,320,1186,320C1157.6,320,1129,320,1101,320C1072.9,320,1045,320,1016,320C988.2,320,960,320,932,320C903.5,320,875,320,847,320C818.8,320,791,320,762,320C734.1,320,706,320,678,320C649.4,320,621,320,593,320C564.7,320,536,320,508,320C480,320,452,320,424,320C395.3,320,367,320,339,320C310.6,320,282,320,254,320C225.9,320,198,320,169,320C141.2,320,113,320,85,320C56.5,320,28,320,14,320L0,320Z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="image-down-layout-chisono"><path class="my-2" fill="#CAF0F8" fill-opacity="1" d="M0,128L14.1,112C28.2,96,56,64,85,80C112.9,96,141,160,169,170.7C197.6,181,226,139,254,101.3C282.4,64,311,32,339,69.3C367.1,107,395,213,424,256C451.8,299,480,277,508,245.3C536.5,213,565,171,593,138.7C621.2,107,649,85,678,96C705.9,107,734,149,762,160C790.6,171,819,149,847,149.3C875.3,149,904,171,932,197.3C960,224,988,256,1016,234.7C1044.7,213,1073,139,1101,106.7C1129.4,75,1158,85,1186,90.7C1214.1,96,1242,96,1271,106.7C1298.8,117,1327,139,1355,170.7C1383.5,203,1412,245,1426,266.7L1440,288L1440,320L1425.9,320C1411.8,320,1384,320,1355,320C1327.1,320,1299,320,1271,320C1242.4,320,1214,320,1186,320C1157.6,320,1129,320,1101,320C1072.9,320,1045,320,1016,320C988.2,320,960,320,932,320C903.5,320,875,320,847,320C818.8,320,791,320,762,320C734.1,320,706,320,678,320C649.4,320,621,320,593,320C564.7,320,536,320,508,320C480,320,452,320,424,320C395.3,320,367,320,339,320C310.6,320,282,320,254,320C225.9,320,198,320,169,320C141.2,320,113,320,85,320C56.5,320,28,320,14,320L0,320Z"></path></svg>
 
-<div class="container py-5  Fifth-main " id="fifth-main">
+<div class="Fifth-main pt-5 " id="fifth-main">
+    <div class="container">
 
-    <div class="row g-0 ">
-        <div class="col column-contact  ">
-            <h1 class="py-4 d-flex justify-content-center pt-sans-bold">CONTA <span class="text-title">TTI</span></h1>
-
-            
-            <div class="d-flex justify-content-center">
-                <i class="fa-solid fa-share-nodes icon-contact"></i>
+        <div class="row g-0 ">
+            <div class="col column-contact  ">
+                <h1 class="py-4 d-flex justify-content-center pt-sans-bold">CONTA <span class="text-title">TTI</span></h1>
+    
+                
+                <div class="d-flex justify-content-center">
+                    <i class="fa-solid fa-share-nodes icon-contact"></i>
+                </div>
+    
+                <div class="py-4 w-100">
+                    <p class="d-flex justify-content-center fs-2 w-100">
+                        <i class="fa-solid fa-envelope pt-2 pe-2"></i> Email: claudiopatti1997@gmail.com
+                    </p>
+                    <p class="d-flex justify-content-center fs-2">
+                        <i class="fa-brands fa-github pt-2 pe-2"></i> Github:&nbsp; <a href="https://github.com/claudiopatti" class="text-decoration-none">Claudio Patti</a>
+                    </p>
+                    <p class="d-flex justify-content-center fs-2">
+                        <i class="fa-brands fa-linkedin pt-2 pe-2"></i> Linkedin:&nbsp; <a href="https://www.linkedin.com/in/claudio-patti1997/" class="text-decoration-none">Claudio Patti</a>
+                    </p>
+                </div>
+    
             </div>
-
-            <div class="py-4 w-100">
-                <p class="d-flex justify-content-center fs-2 w-100">
-                    <i class="fa-solid fa-envelope pt-2 pe-2"></i> Email: claudiopatti1997@gmail.com
-                </p>
-                <p class="d-flex justify-content-center fs-2">
-                    <i class="fa-brands fa-github pt-2 pe-2"></i> Github:&nbsp; <a href="https://github.com/claudiopatti" class="text-decoration-none">Claudio Patti</a>
-                </p>
-                <p class="d-flex justify-content-center fs-2">
-                    <i class="fa-brands fa-linkedin pt-2 pe-2"></i> Linkedin:&nbsp; <a href="https://www.linkedin.com/in/claudio-patti1997/" class="text-decoration-none">Claudio Patti</a>
-                </p>
-            </div>
-
         </div>
+
     </div>
+
 </div>
 
 </template>
@@ -237,22 +244,36 @@ export default {
 
 .First-main {
     height: 500px;
-}
-
-.Second-main{
-    height: 100%;
+    background-color: black;
+    padding-top: 120px;
 }
 
 .Second-main, .Quarter-main {
     height: 100%;
     width: 100%;
     cursor: pointer;
+    background-color: #CAF0F8;
     // margin-top: 30px;
 
     .column-Skills{
         background-color: #CAF0F8;
         height: 100%;
         width: 100%;
+
+        .container-title-skill {
+            height: 100%;
+
+            
+            .title-skill {
+                font-size: 30px;
+                background-color: #00B4D8;
+                border: 10px solid #61a5c2;
+                margin-bottom: 0;
+                height: 100%;
+                
+            }
+        }
+
         .col-6, .col {
             padding: 0;
             // height: 100%;
@@ -265,6 +286,8 @@ export default {
                 font-size: 30px;
                 font-weight: bold;
             }
+
+
             .button-skill-click {
                 border: 10px solid #61a5c2;
                 background-color: #00B4D8;
@@ -300,7 +323,7 @@ export default {
     // }
 
     .column-about {
-        background-color: #CAF0F8;
+        // background-color: #CAF0F8;
         height: 100%;
 
         .description-design{
@@ -338,6 +361,10 @@ export default {
 
 .Fifth-main{
     width: 100%;
+    background-color: white;
+    // transform: translateY(-10px);
+    padding: 20px 0 50px 0;
+
 
     .icon-contact{
         font-size: 200px;
@@ -348,20 +375,8 @@ export default {
         background-color: #CAF0F8;
         height: 100%;
         width: 100%;
-    }
-}
+        border-radius: 40px;
 
-.container-title-skill {
-    height: 100%;
-
-    
-    .title-skill {
-        font-size: 40px;
-        background-color: #00B4D8;
-        border: 10px solid #61a5c2;
-        margin-bottom: 0;
-        height: 100%;
-        
     }
 }
 
@@ -374,22 +389,26 @@ export default {
 }
 
 .image-up-layout-competenze {
-    transform: translateY(10px);
+    // transform: translateY(10px);
+    background-color: black;
 }
 
 .image-down-layout-competenze {
-    // transform: translateY(-10px);
     transform: rotate(180deg);
+    translate: 0 -10px;
     background-color: white;
 }
 
 .image-up-layout-chisono {
-    transform: translateY(15px);
+    transform: translateY(10px);
     background-color: white;
+    padding-top: 20px;
+
 
 }
 
 .image-down-layout-chisono {
+    // transform: translateY(-10px);
     transform: rotate(180deg);
     background-color: white;
 }
